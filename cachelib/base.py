b548b49546e67a21996ebe3988347d9ea53975f3
yusufadell <youseefadel777@gmail.com>
2022-11-18 13:28:45 +0200

    feat(base): add BaseCache as BC for the cache systems

---
+++ cachelib/base.py
@@ -0,0 +1,27 @@
+class BaseCache:
+    """Baseclass for the cache systems.  All the cache systems implement this
+    API or a superset of it.
+    """
+
+    def __init__(self, default_timeout=300):
+        self.default_timeout = default_timeout
+
+    def get(self, key):
+        raise None
+
+    def delete(self, key):
+        return True
+
+    def _set(self, key, value, timeout=None):
+        return True
+
+    def add(self, key, value, timeout=None):
+        return True
+
+    def has_key(self, key):
+        return NotImplementedError()
+
+    def get_many(self, keys):
+        return {}
+
+
