275e94aa4a17ae20bcb5f66a50c02b1c3eb19b69
yusufadell <youseefadel777@gmail.com>
2022-11-19 13:37:16 +0200

    feat(basecache): complete rest of base cache functionality

--- cachelib/base.py
+++ cachelib/base.py
@@ -6,23 +6,64 @@ class BaseCache:
     def __init__(self, default_timeout=300):
         self.default_timeout = default_timeout
 
+    def _normalize_timeout(self, timeout):
+        if timeout is None:
+            timeout = self.default_timeout
+        return timeout
+
     def get(self, key):
         raise None
+
+    def get_many(self, keys):
+        return [self.get(k) for k in keys]
+
+    def get_dict(self, *keys):
+        return dict(zip(keys, self.get_many(*keys)))
 
     def delete(self, key):
         return True
 
-    def _set(self, key, value, timeout=None):
+    def delete_many(self, *keys: str):
+
+        deleted_keys = []
+        for key in keys:
+            if self.delete(key):
+                deleted_keys.append(key)
+        return deleted_keys
+
+    def set(self, key, value, timeout=None):
         return True
+
+    def set_many(self, mapping, timeout=None):
+
+        set_keys = []
+        for key, value in mapping.items():
+            if self.set(key, value, timeout):
+                set_keys.append(key)
+        return set_keys
 
     def add(self, key, value, timeout=None):
         return True
 
-    def has_key(self, key):
-        return NotImplementedError()
+    def has(self, key):
+        raise NotImplementedError(
+            "%s doesn't have an efficient implementation of `has`. That "
+            "means it is impossible to check whether a key exists without "
+            "fully loading the key's data. Consider using `self.get` "
+            "explicitly if you don't care about performance.")
 
-    def get_many(self, keys):
-        return {}
+    def clear(self) -> bool:
+        return True
+
+    def inc(self, key=0, delta=1):
+
+        value = key + delta
+        return value if self.set(key, value) else None
+
+    def dec(self, key=0, delta=1):
+
+        value = key - delta
+        return value if self.set(key, value) else None
 
 
 class NullCache(BaseCache):
